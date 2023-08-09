755148e6403adbae7ce3304a7df9926df789874e
Loyd Ngei <loydngei@gmail.com>
2023-08-10 00:26:26 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,15 +6,13 @@ from typing import List, TypeVar
 
 class Auth:
     """Class name auth that takes in public methods"""
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool: 
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method require_auth"""
         return False
-
 
     def authorization_header(self, request=None) -> str:
         """Method authorization header"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Method Current user"""
