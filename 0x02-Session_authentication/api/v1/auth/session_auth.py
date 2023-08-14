b3a1c9d19854568f3ad9f94af2e374e531149d3b
Loyd Ngei <loydngei@gmail.com>
2023-08-14 17:15:17 +0300

    Session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,25 @@
 """A new class SessionAuth inherits from Auth
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """A new class SessionAuth inherits from Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Instance that creates a Session ID for a user id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Instance method that returns a User ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
