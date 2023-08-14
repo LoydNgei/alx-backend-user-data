43d848f548c0f3b340cdf3801e8b5db1344bb3b5
Loyd Ngei <loydngei@gmail.com>
2023-08-14 16:47:05 +0300

    Session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A new class SessionAuth inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """A new class SessionAuth inherits from Auth
+    """
+    pass
