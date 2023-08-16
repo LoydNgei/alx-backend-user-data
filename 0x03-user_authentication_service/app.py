c3990368d1b0269650c16a850f7c599231254dee
Loyd Ngei <loydngei@gmail.com>
2023-08-16 20:35:45 +0300

    Flask App

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,10 +6,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/", methods='GET')
-def flask_app():
-    result = {"message": "Bienvenue"}
-    return jsonify(result)
+@app.route('/', methods=['GET'])
+def flask_app() -> str:
+    """The JSON route"""
+    return jsonify({"message": "Bienvenue"})
 
 
 if __name__ == "__main__":
