db2ee0658bd3a5c9f85030511cdfd16a8283a820
Loyd Ngei <loydngei@gmail.com>
2023-08-18 00:52:53 +0300

    Updates

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,5 +44,51 @@ def session_login() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """Logout from a session"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'])
+def user_profile() -> str:
+    """Implement a profile function"""
+    profile = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(profile)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """The Get reset password token method"""
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except Exception:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """Update password method"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,7 +55,6 @@ class Auth:
             user.session_id = _generate_uuid()
             return user.session_id
 
-
     def get_user_from_session_id(self, session_id: str) -> User:
         """Find user by session ID"""
         if session_id is None:
@@ -63,6 +62,37 @@ class Auth:
         try:
             user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
-            return None  
+            return None
         else:
             return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy session Method"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset Password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password method"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
